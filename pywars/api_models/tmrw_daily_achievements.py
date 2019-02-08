b5e6ec2e5f570aa59a32179d7985579f220cd3a1
jonesd <jonesd@uvic.ca>
2019-02-08 00:40:47 -0800

    separate tmrw_daily_achievements API model into its own file

--- pywars/api_models/tmrw_daily_achievements.py
+++ pywars/api_models/tmrw_daily_achievements.py
@@ -1,10 +1,10 @@
 import json
 
-class Daily_Achievements:
+class Tmrw_Daily_Achievements:
     def __init__(self, json):
         self.id = json['id']
         self.level = json['level']
         self.required_access = json['required_access']
-
+    
     def __str__(self):
         return "ID: {0}\nLevel: {1}\nRequired_access: {2}".format(self.id, self.level, self.required_access)
